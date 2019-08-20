5ba37b98c9fa2933b4f936ac44bf22cc327fb673
root <root@localhost.localdomain>
2019-08-20 18:12:06 +0100

    commit

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -439,30 +439,30 @@ class ShipmentController extends Controller
 
     public function updateStatus(Request $request, Shipment $shipment, $id)
     {
-        // try {
-        //     $client = new Client;
-        //     $request = $client->request('POST', env('API_URL') . '/api/orderStatus', [
-        //         'headers' => [
-        //             'Content-type' => 'application/json',
-        //             'Accept' => 'application/json',
-        //             'Authorization' => 'Bearer ' . $this->token_f(),
-        //         ],
-        //         'body' => json_encode([
-        //             'data' => $request->all(),
-        //         ])
-        //     ]);
-        //     // $response = $http->get(env('API_URL').'/api/getUsers');
-        //     return $response = $request->getBody()->getContents();
-        // } catch (\Exception $e) {
-        //     \Log::error($e->getMessage() . ' ' . $e->getLine() . ' ' . $e->getFile());
-        //     // return $e->getMessage() . ' ' . $e->getLine() . ' ' . $e->getFile();
-        //     $code = $e->getResponse()->getStatusCode();
-        //     if ($code == 401) {
-        //         abort(401);
-        //     }
-        //     $message = $e->getResponse()->getBody();
-        //     abort(422, $message);
-        // }
+        try {
+            $client = new Client;
+            $request = $client->request('POST', env('API_URL') . '/api/orderStatus', [
+                'headers' => [
+                    'Content-type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . $this->token_f(),
+                ],
+                'body' => json_encode([
+                    'data' => $request->all(),
+                ])
+            ]);
+            // $response = $http->get(env('API_URL').'/api/getUsers');
+            return $response = $request->getBody()->getContents();
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage() . ' ' . $e->getLine() . ' ' . $e->getFile());
+            // return $e->getMessage() . ' ' . $e->getLine() . ' ' . $e->getFile();
+            $code = $e->getResponse()->getStatusCode();
+            if ($code == 401) {
+                abort(401);
+            }
+            $message = $e->getResponse()->getBody();
+            abort(422, $message);
+        }
         $this->update_status($request->all());
         // return $request->all();
         $no = $request->formobg['client_phone'];
